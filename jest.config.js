7f462c6d0309c7d5c60aea97057b3d85cca34a00
norabraskerud <norabraskerud@gmail.com>
2023-04-05 22:28:18 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.js'],
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
   testPathIgnorePatterns: ['/node_modules/'],
   transform: {
-    '^.+.jsx?$': 'babel-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
   collectCoverage: true,
   coverageDirectory: 'coverage',
