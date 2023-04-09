99816c84b73f702763ed7d704d95179a07ed5c06
norabraskerud <norabraskerud@gmail.com>
2023-04-09 14:08:38 +0200

    added back

---
+++ unit-tests/login.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+export function login(email, password) {
+    // code to perform login logic
+    return { token: 'abc123' };
+  }
---
+++ unit-tests/logout.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import 'jest-localstorage-mock';
+
+describe('Logout', () => {
+  const logout = () => {
+    localStorage.removeItem('token');
+  };
+
+  beforeEach(() => {
+    localStorage.setItem('token', '1234');
+  });
+
+  it('should clear the token from local storage on logout', () => {
+    logout();
+    expect(localStorage.getItem('token')).toBeNull();
+  });
+});
